16921a807bf75878c21733ed20f3b4a24d9962b4
Giovanni Leo <giovannileo100@gmail.com>
2024-09-19 20:55:59 +0800

    translate options

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,22 @@ import SearchLanguage from "./components/SearchLanguage";
 const App = () => {
   const [languages, setLanguages] = useState({ from: "en", to: "en" });
 
+  console.log(languages);
+
   return (
     <div className="w-full max-w-[1440px] mx-auto p-5">
-      <SearchLanguage />
+      <div className="flex w-full gap-5">
+        <SearchLanguage
+          setLanguage={setLanguages}
+          dest={"from"}
+          title="Translate From"
+        />
+        <SearchLanguage
+          setLanguage={setLanguages}
+          dest={"to"}
+          title="Translate To"
+        />
+      </div>
     </div>
   );
 };
--- src/components/SearchLanguage.jsx
+++ src/components/SearchLanguage.jsx
@@ -1,33 +1,38 @@
 import React, { useState } from "react";
-import LangugeButton from "./LangugeButton";
 import { languages } from "../constants";
 
-const SearchLanguage = () => {
-  const [searchKey, setSearchKey] = useState("");
+const SearchLanguage = ({ setLanguage, dest, title }) => {
+  const [selectedLanguage, setSelectedLanguage] = useState("");
 
-  const handleInputChange = (event) => {
-    setSearchKey(event.target.value);
+  const handleSelectChange = (event) => {
+    const { value, selectedOptions } = event.target;
+    const selectedOption = selectedOptions[0];
+    const code = selectedOption.getAttribute("data-code");
+
+    setSelectedLanguage(value);
+    setLanguage((prev) => ({ ...prev, [dest]: code })); // Update the language code
   };
 
   return (
-    <div>
-      <input
+    <div className="w-full">
+      <select
         className="w-full border-[1px] py-1 pl-4 rounded-sm outline-none transition-all duration-200 focus:border-blue-500"
-        type="text"
-        value={searchKey}
-        onChange={handleInputChange}
-        placeholder="Search languages"
-      />
-
-      <div className="flex flex-wrap gap-2 mt-5">
-        {languages
-          .filter((language) =>
-            language.name.toLowerCase().includes(searchKey.toLowerCase())
-          )
-          .map((language) => (
-            <LangugeButton key={language.code} name={language.name} />
-          ))}
-      </div>
+        value={selectedLanguage}
+        onChange={handleSelectChange}
+      >
+        <option value="" disabled>
+          {title}
+        </option>
+        {languages.map((language) => (
+          <option
+            key={language.code}
+            value={language.name}
+            data-code={language.code}
+          >
+            {language.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
