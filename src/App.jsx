594dbad8c219fb1ff1028a1a362b3e59be24a60a
Giovanni Leo <giovannileo100@gmail.com>
2024-09-19 21:23:24 +0800

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LanguageBox from "./components/LanguageBox";
 
 const App = () => {
   const [languages, setLanguages] = useState({ from: "en", to: "en" });
+  const [input, setInput] = useState("");
   const [translated, setTranslated] = useState("");
 
   async function translate() {
@@ -12,14 +13,14 @@ const App = () => {
     const options = {
       method: "POST",
       headers: {
-        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-key": import.meta.env.VITE_API_URL,
         "x-rapidapi-host": "google-api31.p.rapidapi.com",
         "Content-Type": "application/json",
       },
       body: {
-        text: "William Edward Sanders (1883–1917) was a New Zealand recipient of the Victoria Cross (VC), the highest British and Commonwealth combat award for gallantry. He took up a seafaring career in 1899 and earned a master's certificate in 1914, after the outbreak of World War I. He served on troopships until April 1916, when he was commissioned in the Royal Naval Reserve. He was appointed second in command of Helgoland, a Q-ship operating against German submarines. He was given his own command, HMS Prize, in February 1917. Sanders was awarded the VC for his actions while on his first patrol as captain, when Prize engaged and drove off a German U-boat that had attacked and damaged the ship. He was killed in action when Prize was sunk by a U-boat on her fourth patrol. His VC is currently held by the Auckland War Memorial Museum. His memorials include the Sanders Cup, a sailing trophy for 14-foot (4.3 m) yachts.",
-        to: "fr",
-        from_lang: "",
+        text: input,
+        to: languages.to,
+        from_lang: languages.from,
       },
     };
 
@@ -49,12 +50,15 @@ const App = () => {
       <div className="flex w-full gap-5 mt-5">
         <textarea
           className="w-full border-[1px] p-5 focus:outline-blue-400"
-          name=""
-          id=""
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
         ></textarea>
         <LanguageBox text={translated} />
       </div>
-      <button className="w-full bg-blue-400 text-white mt-20 py-2 rounded-md hover:opacity-50 duration-500 ease-in">
+      <button
+        onClick={translate}
+        className="w-full bg-blue-400 text-white mt-20 py-2 rounded-md hover:opacity-50 duration-500 ease-in"
+      >
         Translate
       </button>
     </div>
