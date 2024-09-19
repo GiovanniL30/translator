e0a5823444282993122a75291e2371dcc986024c
Giovanni Leo <giovannileo100@gmail.com>
2024-09-19 20:12:04 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+const App = () => {
+  const [languages, setLanguages] = useState({ from: "en", to: "en" });
+
+  return <div>App</div>;
+};
+
+export default App;
